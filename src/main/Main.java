efaece444190ee3c869a6e0839f3311794c16791
Aboele13 <andrew.boele@yahoo.com>
2023-01-09 15:46:57 -0800

    started adding gamepanel and stuff

---
+++ src/main/Main.java
@@ -0,0 +1,25 @@
+package Main;
+
+import javax.swing.JFrame;
+
+public class main {
+
+    public static void main(String[] args) {
+        
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("w102");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
+        
+    }
+}
