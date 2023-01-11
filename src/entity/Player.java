bff4a42cca498639e2ec190e4c8bbb32365247d9
Aboele13 <andrew.boele@yahoo.com>
2023-01-10 17:49:47 -0800

    improving movement up to 11:00 in video 3

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,5 +1,70 @@
 package entity;
 
+import main.GamePanel;
+import main.KeyHandler;
+import java.awt.Graphics2D;
+import java.io.IOException;
+import java.awt.Color;
+import java.awt.IO;
+
 public class Player extends Entity {
 
+    GamePanel gp;
+    KeyHandler keyH;
+
+    public Player(GamePanel gp, KeyHandler keyH) {
+
+        this.gp = gp;
+        this.keyH = keyH;
+
+        setDefaultValues();
+    }
+
+    public void setDefaultValues() {
+
+        x = 100;
+        y = 100;
+        speed = 4;
+        direction = "down";
+    }
+
+    public void getPlayerImage() {
+
+        try {
+
+            up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_dowm_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void draw(Graphics2D g2) {
+        g2.setColor(Color.white);
+
+        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+    }
+
+    public void update() {
+        if (keyH.upPressed) {
+            y -= speed;
+        }
+        if (keyH.downPressed) {
+            y += speed;
+        }
+        if (keyH.leftPressed) {
+            x -= speed;
+        }
+        if (keyH.rightPressed) {
+            x += speed;
+        }
+    }
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -5,7 +5,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-
+import entity.Player;
 
 public class GamePanel extends JPanel implements Runnable {
 
@@ -14,7 +14,7 @@ public class GamePanel extends JPanel implements Runnable {
     final int scale = 3;
     final int FPS = 60;
 
-    final int tileSize = originalTileSize * scale; // 48x48 tile
+    public final int tileSize = originalTileSize * scale; // 48x48 tile
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol; // 769 pixels
@@ -22,6 +22,7 @@ public class GamePanel extends JPanel implements Runnable {
 
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    Player player = new Player(this, keyH);
 
     //default position
     int playerX = 100;
@@ -43,29 +44,16 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void update() {
-        if (keyH.upPressed) {
-            playerY -= playerSpeed;
-        }
-        if (keyH.downPressed) {
-            playerY += playerSpeed;
-        }
-        if (keyH.leftPressed) {
-            playerX -= playerSpeed;
-        }
-        if (keyH.rightPressed) {
-            playerX += playerSpeed;
-        }
+        player.update();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D)g;
-
-        g2.setColor(Color.white);
-
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
     
+        player.draw(g2);
+
         g2.dispose();
 
     }
@@ -74,30 +62,23 @@ public class GamePanel extends JPanel implements Runnable {
     public void run() {
 
         double drawInterval = 1000000000/FPS; 
-        double nextDrawTime = System.nanoTime() + drawInterval;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
 
         while (gameThread != null) {
 
-            update();
+            currentTime = System.nanoTime();
 
-            repaint();
+            delta += (currentTime - lastTime) / drawInterval;
 
-            try {
-                double remainingTime = nextDrawTime - System.nanoTime();
-                remainingTime = remainingTime/1000000;
+            lastTime = currentTime;
 
-                if (remainingTime < 0) {
-                    remainingTime = 0;
-                }
-
-                Thread.sleep((long)remainingTime);
-
-                nextDrawTime += drawInterval;
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
             }
-
         }
 
     }
